c668b6c5f25f84e72e41b525afd10999bd2a2ec3
ThreeDeeJay <71472458+ThreeDeeJay@users.noreply.github.com>
2025-08-28 17:58:52 -0400

    Update [HeSuVi] Find and install HRIRs.cmd
    Copies HRIRs into the More folder so new ones aren't added to the Common section, and excludes copying the ones in the [HeSuVi] folder since they're assumed to be already installed with HeSuVi

--- [HeSuVi] Find and install HRIRs.cmd
+++ [HeSuVi] Find and install HRIRs.cmd
@@ -1,12 +1,20 @@
-@ECHO OFF
+@echo off
+SetLocal EnableExtensions
+SetLocal EnableDelayedExpansion
+
 pushd "%~dp0"
 FOR /F "tokens=2* skip=2" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\EqualizerAPO" /v "InstallPath" 2^>^>^"NUL^"') do SET "HeSuViPath=%%b\Config\HeSuVi"
 	ECHO HeSuVi path detected: "%HeSuViPath%"
 	ECHO.
-	IF EXIST "%HeSuViPath%\hrir" (
+	IF EXIST "%HeSuViPath%\hrir\more" (
 		FOR /D /R %%D in (*) DO (
-			IF "%%~nxD"=="44" (
-				xcopy "%%~dpD" "%HeSuViPath%\hrir" /s /i /y
+			Set "HRIRPath=%%D"
+			If "!HRIRPath!"=="!HRIRPath:[HeSuVi]=!" (
+				IF EXIST "!HRIRPath!\44" (
+					Echo %%~nxD
+					robocopy "!HRIRPath!\44"	"%HeSuViPath%\hrir\44\more"	*.wav /MOVE /NS /NC /NFL /NDL /NJH /NJS >NUL
+					robocopy "!HRIRPath!"		"%HeSuViPath%\hrir\more"	*.wav /MOVE /NS /NC /NFL /NDL /NJH /NJS >NUL
+					)
 				)
 			)
 		) ELSE (
